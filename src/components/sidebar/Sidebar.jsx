96d93d0fad901fb647401f7a614e251842dc0dcf
erickpjoshy <erickpjoshy@gmail.com>
2024-05-27 12:01:13 +0530

    second commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import './Sidebar.scss';
+import './sidebar.scss';
 import { motion } from 'framer-motion';
 import Tooglebutton from './tooglebuttons/Tooglebutton';
 import Links from './links/links';
